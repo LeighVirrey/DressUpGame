1a1587ca9c5097b672cd9ac0e489fb8b15935068
TOONZK <126704941+LeighVirrey@users.noreply.github.com>
2024-02-14 20:36:56 -0700

    file injections
    maybe fix?

---
+++ DressUpGame/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DressUpGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
