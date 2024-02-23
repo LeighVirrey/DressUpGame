a1274aa91f2751fbf4483771c7207edac9cecd8a
Johanna Johnson <johjohnson@student.neumont.edu>
2024-02-22 21:39:38 -0700

    The button now work, YAY

--- DressUpGame/Controllers/StoryController.cs
+++ DressUpGame/Controllers/StoryController.cs
@@ -6,78 +6,60 @@ namespace DressUpGame.Controllers
 {
     public class StoryController : Controller
     {
-        private List<Scene> storyScenes = new List<Scene>(); 
 
-        private int currentSceneIndex = 0; // Current index to keep track of the current scene
 
-        public ActionResult Index()
+        
+        //private List<Scene> storyScenes = new List<Scene>(); 
+
+        //private int currentSceneIndex = 0; // Current index to keep track of the current scene
+
+        public ActionResult StartGame()
         {
-            // Load your initial scene
-            Scene currentScene = storyScenes[currentSceneIndex];
-            return View(currentScene);
+           return View();
         }
+
 
         public ActionResult Next()
         {
-            if (currentSceneIndex < storyScenes.Count - 1)
-            {
-                currentSceneIndex++;
-            }
-
-            Scene nextScene = storyScenes[currentSceneIndex];
-            return View("Next", nextScene);
+            return View("Page1");
         }
 
         public ActionResult Prev()
         {
-            if (currentSceneIndex > 0)
-            {
-                currentSceneIndex--;
-            }
-
-            Scene prevScene = storyScenes[currentSceneIndex];
-            return View("Prev", prevScene);
+            return View("StartGame");
         }
+        public ActionResult Page2()
+        {
+            return View("Page3");
+        }
+        public ActionResult Page3()
+        {
+            return View("Page4");
+        }
+        public ActionResult Page5()
+        {
+            return View("Page6");
+        }
+        public ActionResult Page6()
+        {
+            return View("Page7");
+        }
+        public ActionResult Page7()
+        {
+            return View("Page8");
+        }
+        public ActionResult Page8()
+        {
+            return View("Page9");
+        }
+
+
+
+
     }
 
 }
-    //{
-    //    private List<Scene> scenes = new List<Scene>
-    //{
-    //    new Scene { Id = 1, Content = "This is scene 1." },
-    //    new Scene { Id = 2, Content = "This is scene 2." },
-    //    // Add more scenes as needed
-    //};
-
-    //    public ActionResult Index(int id = 1)
-    //    {
-    //        Scene scene = scenes.FirstOrDefault(s => s.Id == id);
-    //        int totalScenes = scenes.Count;
-    //        ViewBag.TotalScenes = totalScenes;
-    //        return View(scene);
-    //    }
-
-    //    public ActionResult Next(int id)
-    //    {
-    //        Scene nextScene = scenes.FirstOrDefault(s => s.Id == id + 1);
-    //        if (nextScene != null)
-    //            return RedirectToAction("StartScene", new { id = nextScene.Id });
-    //        else
-    //            return RedirectToAction("Index", new { id });
-    //    }
-
-    //    public ActionResult Prev(int id)
-    //    {
-    //        Scene prevScene = scenes.FirstOrDefault(s => s.Id == id - 1);
-    //        if (prevScene != null)
-    //            return RedirectToAction("Index", new { id = prevScene.Id });
-    //        else
-    //            return RedirectToAction("Index", new { id });
-    //    }
-
-
-        
-//}
+   
 
 
 
--- DressUpGame/Models/Scene.cs
+++ DressUpGame/Models/Scene.cs
@@ -2,7 +2,7 @@
 {
     public class Scene
     {
-        public int? Id { get; set; }
+        public int Id { get; set; }
 
         
        
