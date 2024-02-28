2a64bf698331e024a9df36849a8187433b3b82e4
MarcusReyes619 <129239355+MarcusReyes619@users.noreply.github.com>
2024-02-27 18:16:33 -0700

    clothing and character added
    AHAHAHAHA

--- DressUpGame/Models/Character.cs
+++ DressUpGame/Models/Character.cs
@@ -4,6 +4,28 @@
 	{
 		public string Image { get; set; }
 
+		public string Name { get; set; }
 
+		public Clothing? Hat { get; set; }
+		public Clothing? Hair { get; set; }
+		public Clothing? Shirt { get; set; }
+		public Clothing? Shoes { get; set; }
+		public Clothing Pants { get; set; }
+
+		public Character()
+		{
+
+		}
+
+		public Character(string image, string name, Clothing? hat, Clothing? hair, Clothing? shirt, Clothing? shoes, Clothing pants)
+		{
+			this.Image = image;
+			this.Name = name;
+			this.Hat = hat;
+			this.Hair = hair;
+			this.Shirt = shirt;
+			this.Shoes = shoes;
+			this.Pants = pants;
+		}
 	}
 }
